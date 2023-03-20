d24d24361eb774ef214514d59885634106ffcec2
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-03-20 16:55:09 +0100

    Project Folder Setup

--- admin/index.ts
+++ admin/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8003;
 
 app.listen(PORT, () => {
     console.log(`User service is running on PORT ${PORT}`)
-});
+})
--- lecturer/index.ts
+++ lecturer/index.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 8002;
 
 app.listen(PORT, () => {
     console.log(`User service is running on PORT ${PORT}`)
-});
+})
