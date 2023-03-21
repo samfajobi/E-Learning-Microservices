2aca0a1df0be2279b3869c69f27bd390a5066838
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-03-21 19:21:06 +0100

    Added Proxy

---
+++ gateway/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import proxy from "express-http-proxy";
+
+const app = express();
+
+
+
+app.use("/user", proxy("http://localhost:8000"));
+app.use("/user", proxy("http://localhost:8000"));
+app.use("/user", proxy("http://localhost:8000"));
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`User service is running on PORT ${PORT}`)
+});
