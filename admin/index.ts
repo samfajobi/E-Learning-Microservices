33521e56bc15bfd61f6cca0b1cd73ce0e28529b8
Fajobi Olusegun <segzyfaj1@gmail.com>
2023-03-21 19:07:30 +0100

    Added Gateway Proxy

--- admin/index.ts
+++ admin/index.ts
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 8003;
 
 app.listen(PORT, () => {
     console.log(`User service is running on PORT ${PORT}`)
-})
+});
--- lecturer/index.ts
+++ lecturer/index.ts
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 8002;
 
 app.listen(PORT, () => {
     console.log(`User service is running on PORT ${PORT}`)
-})
+});
